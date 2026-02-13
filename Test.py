f63247b2f77ac7738b1cd2518f8aad0d3ff66e89
vinay592 <71247370+vinay592@users.noreply.github.com>
2026-02-14 01:37:46 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,8 +3,10 @@ import unittest
 from Prog1 import summation
 
 class TestSum(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(summation([10, 20]), 30)
+    def test_list_int(self):
+        data = [23, 32]
+        result = summation(data)
+        self.assertEqual(result, 55)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
